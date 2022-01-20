5816bc97c7d8bce4bedc7b797816f8d3a4329688
Bharath Subu <bharathsubu2002@gmial.com>
2022-01-20 22:49:23 +0530

    heroku deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 //Creating a connection
 
